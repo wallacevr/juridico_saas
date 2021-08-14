7396a7a283fe2f2c1248995bf93e61ba40b9e82f
Humberto <setando@gmail.com>
2021-08-14 02:18:56 +0200

    Create table to customers

---
+++ database/migrations/tenant/2021_08_13_102951_create_customers_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->date('dob')->default(null)->nullable();
+            $table->string('taxvat')->unique()->nullable();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->integer('failures_num')->default(0);
+            $table->date('first_failure')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+}
