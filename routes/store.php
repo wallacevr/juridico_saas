0a08801ae71d31b49f0ddf6f2ce590baed5e6ecc
Humberto <setando@gmail.com>
2021-08-20 02:33:23 +0200

    add admin to Tenant

--- routes/store.php
+++ routes/store.php
@@ -11,30 +11,35 @@ Route::group([
     'middleware' => ['tenant', PreventAccessFromCentralDomains::class], // See the middleware group in Http Kernel
     'as' => 'store.',
 ], function () {
-
-    Route::middleware('guest:customers')->group(function(){
-        //login route
-        Route::get('/login','LoginController@login')->name('customer.login');
-        Route::post('/login','LoginController@processLogin');
-
-        Route::get('/register', 'CustomerController@show')->name('customer.register');
-        Route::post('/register/submit', 'CustomerController@submit')->name('customer.register.submit');
-    });
-
-    Route::middleware('auth:customers')->group(function(){
-
-        Route::post('/logout',function(){
-            Auth::guard('customers')->logout();
-            return redirect()->action([
-                HomeController::class,
-                'index'
-            ]);
-        })->name('logout');
-
-    });
-
-
+    Route::prefix('customer')->group(function () {
+        
+        Route::middleware('guest:customers')->group(function(){
+            //login route
+            Route::get('/login','LoginController@login')->name('customer.login');
+            Route::post('/login','LoginController@processLogin');
     
-
+            Route::get('/register', 'CustomerController@show')->name('customer.register');
+            Route::post('/register/submit', 'CustomerController@submit')->name('customer.register.submit');
+        });
+    
+        Route::middleware('auth:customers')->group(function(){
+    
+            Route::post('/logout',function(){
+                Auth::guard('customers')->logout();
+                return redirect()->action([
+                    HomeController::class,
+                    'index'
+                ]);
+            })->name('logout');
+    
+        });
+    
+    
+        
+    
+        
+        
+    });
     Route::get('/', 'HomeController@index')->name('store.index');
+    
 });
--- routes/tenant.php
+++ routes/tenant.php
@@ -11,31 +11,52 @@ Route::group([
     'middleware' => ['tenant', PreventAccessFromCentralDomains::class], // See the middleware group in Http Kernel
     'as' => 'tenant.',
 ], function () {
+
+    $admin = config('nova.path');
+
     Route::get('/impersonate/{token}', function ($token) {
         return UserImpersonation::makeResponse($token);
     })->name('impersonate');
+    
+    
 
-    Auth::routes();
+    Route::prefix($admin)->group(function () {
+        Route::any('{user?}/{s?}',function(){
+            abort(404);
+        });
+    });
+    
 
     Route::post('/ploi/webhook/certificateIssued', 'PloiWebhookController@certificateIssued')->name('ploi.certificate.issued');
     Route::post('/ploi/webhook/certificateRevoked', 'PloiWebhookController@certificateRevoked')->name('ploi.certificate.revoked');
-
-    Route::middleware(['auth', CheckSubscription::class])->group(function () {
-        Route::redirect('/home', '/posts')->name('home');
-
-        Route::get('/posts', 'PostController@index')->name('posts.index');
-        Route::post('/posts', 'PostController@store')->name('posts.store');
-        Route::get('/posts/create', 'PostController@create')->name('posts.create');
-        Route::get('/posts/{post}', 'PostController@show')->name('posts.show');
-
-        Route::get('/settings/user', 'UserSettingsController@show')->name('settings.user');
-        Route::post('/settings/user/personal', 'UserSettingsController@personal')->name('settings.user.personal');
-        Route::post('/settings/user/password', 'UserSettingsController@password')->name('settings.user.password');
-
-        Route::middleware(OwnerOnly::class)->group(function () {
-            Route::get('/settings/application', 'ApplicationSettingsController@show')->name('settings.application');
-            Route::post('/settings/application/configuration', 'ApplicationSettingsController@storeConfiguration')->name('settings.application.configuration');
-            Route::get('/settings/application/invoice/{id}/download', 'DownloadInvoiceController')->name('invoice.download');
+    
+   
+    Route::prefix('admin')->group(function () {
+        Auth::routes();
+       
+        Route::middleware('guest:web')->group(function(){
+            //login route
+            Route::redirect('/','/admin/login');
+            
+        });
+        Route::middleware(['auth', CheckSubscription::class])->group(function () {
+            Route::redirect('/','/dashboard');
+            Route::get('/dashboard', 'ApplicationSettingsController@show')->name('admin.dashboad');
+            Route::get('/posts', 'PostController@index')->name('posts.index');
+            Route::post('/posts', 'PostController@store')->name('posts.store');
+            Route::get('/posts/create', 'PostController@create')->name('posts.create');
+            Route::get('/posts/{post}', 'PostController@show')->name('posts.show');
+    
+            Route::get('/settings/user', 'UserSettingsController@show')->name('settings.user');
+            Route::post('/settings/user/personal', 'UserSettingsController@personal')->name('settings.user.personal');
+            Route::post('/settings/user/password', 'UserSettingsController@password')->name('settings.user.password');
+    
+            Route::middleware(OwnerOnly::class)->group(function () {
+                Route::get('/settings/application', 'ApplicationSettingsController@show')->name('settings.application');
+                Route::post('/settings/application/configuration', 'ApplicationSettingsController@storeConfiguration')->name('settings.application.configuration');
+                Route::get('/settings/application/invoice/{id}/download', 'DownloadInvoiceController')->name('invoice.download');
+            });
         });
     });
+    
 });
