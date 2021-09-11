bdecfe8cb2651b502842f48e28008a79c3b2d29c
Lucas Vinicius <lucasvscs@outlook.com>
2021-09-11 00:13:52 -0300

    Creating alerts layout and Collections validations

--- app/Http/Controllers/Tenant/CollectionController.php
+++ app/Http/Controllers/Tenant/CollectionController.php
@@ -6,6 +6,7 @@ use App\Collection;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Str;
 
 class CollectionController extends Controller
@@ -38,41 +39,7 @@ class CollectionController extends Controller
         return view('tenant.collections.edit')->with(['collection' => $collection]);
     }
 
-    // Update a Collection
-    // TODO - Criar o delete da imagem antiga e validação de erro
-    // TODO -  Gerar as imagem em uma pasta única para cada tenant?
-    public function update(Request $request, Collection $collection)
-    {
-        $this->validate($request, [
-            'name' => 'required',
-            'page_title' => 'required',
-            'image_url' => 'image|mimes:jpeg,png,jpg,gif,svg|max:10000',
-            'slug' => 'required',
-        ]);
-
-        $data = $request->all();
-        $data['slug'] = $this->generateSlug($data['slug']);
-
-        if (!empty($data['image_url'])) {
-            if ($data['image_url'] !== $collection->image_url && $image = $request->file('image_url')) {
-                $destinationPath = public_path() . '/images';
-                $microtime = preg_replace('/(0)\.(\d+) (\d+)/', '$3$1$2', microtime());
-                $profileImage = Str::random(15) . $microtime . "." . $image->getClientOriginalExtension();
-
-                $image->move($destinationPath, $profileImage);
-                $data['image_url'] = "$profileImage";
-            }
-        }
-
-        $collection->update($data);
-
-        return redirect()
-            ->route('tenant.collections.index')
-            ->with('success', 'Collection updated successfully');
-    }
-
     // Store a Collection
-    // TODO -  Criar a validação de erro
     // TODO -  Gerar as imagem em uma pasta única para cada tenant?
     public function store(Request $request)
     {
@@ -83,36 +50,91 @@ class CollectionController extends Controller
             'slug' => 'required',
         ]);
 
+        $response = [
+            "status" => "success",
+            "message" => "Collection created successfully!",
+        ];
+
         $data = $request->all();
+        $image = $request->file('image_url');
         $data['slug'] = $this->generateSlug($data['slug']);
 
-        if ($image = $request->file('image_url')) {
-            $destinationPath = public_path() . '/images';
-            $microtime = preg_replace('/(0)\.(\d+) (\d+)/', '$3$1$2', microtime());
-            $profileImage = Str::random(15) . $microtime . "." . $image->getClientOriginalExtension();
+        if ($imageUrl = $this->storeImage($image)) {
+            $data['image_url'] = $imageUrl;
 
-            $image->move($destinationPath, $profileImage);
-            $data['image_url'] = "$profileImage";
+            $collection = Collection::create($data);
+
+            if (!$collection->save()) {
+                $response['status'] = "error";
+                $response['message'] = "Error creating collection.";
+            }
         }
-
-        $collection = Collection::create($data);
-
-        $collection->save();
 
         return redirect()
             ->route('tenant.collections.index')
-            ->with('success', 'Collection created successfully!');
+            ->with($response['status'], $response['message']);
+    }
+
+    // Update a Collection
+    // TODO -  Gerar as imagem em uma pasta única para cada tenant?
+    public function update(Request $request, Collection $collection)
+    {
+        $this->validate($request, [
+            'name' => 'required',
+            'page_title' => 'required',
+            'image_url' => 'image|mimes:jpeg,png,jpg,gif,svg|max:10000',
+            'slug' => 'required',
+        ]);
+
+        $response = [
+            "status" => "success",
+            "message" => "Collection deleted successfully!",
+        ];
+
+        $data = $request->all();
+        $image = $request->file('image_url');
+        $data['slug'] = $this->generateSlug($data['slug']);
+
+        if (!empty($data['image_url']) && $data['image_url'] !== $collection->image_url) {
+            if ($imageUrl = $this->storeImage($image)) {
+                $data['image_url'] = $imageUrl;
+
+                $oldImage = public_path() . '/images/' . $collection->image_url;
+
+                File::exists($oldImage) ? File::delete($oldImage) : '';
+
+                if (!$collection->update($data)) {
+                    $response['status'] = "error";
+                    $response['message'] = "Error creating collection.";
+                }
+            }
+        }
+
+        return redirect()
+            ->route('tenant.collections.index')
+            ->with('success', 'Collection updated successfully');
     }
 
     // Delete a Collection
-    // TODO -  Criar o delete da imagem e validação de erro
     public function destroy(Collection $collection)
     {
-        $collection->delete();
+        $response = [
+            "status" => "success",
+            "message" => "Collection deleted successfully!",
+        ];
+
+        $imageUrl = public_path() . '/images/' . $collection->image_url;
+
+        if ($collection->delete()) {
+            File::exists($imageUrl) ? File::delete($imageUrl) : '';
+        } else {
+            $response['status'] = "error";
+            $response['message'] = "Error deleting collection.";
+        }
 
         return redirect()
             ->route('tenant.collections.index')
-            ->with('success', 'Collection deleted successfully!');
+            ->with($response['status'], $response['message']);
     }
 
     // Generate a slug after receiving an string as paramn
@@ -129,5 +151,16 @@ class CollectionController extends Controller
         }
 
         return $newSlug;
+    }
+
+    public function storeImage($image)
+    {
+        $destinationPath = public_path() . '/images';
+        $microtime = preg_replace('/(0)\.(\d+) (\d+)/', '$3$1$2', microtime());
+        $imageUrl = Str::random(15) . $microtime . "." . $image->getClientOriginalExtension();
+
+        $image->move($destinationPath, $imageUrl);
+
+        return $imageUrl;
     }
 }
