f0b07a595f74e143eabf799b67cc846637688941
Lucas Vinicius <lucasvscs@outlook.com>
2021-10-19 22:26:49 -0300

    Brand CRUD validation, refactoring and translations

--- resources/views/tenant/brands/create.blade.php
+++ resources/views/tenant/brands/create.blade.php
@@ -2,36 +2,28 @@
 <link rel="stylesheet" href="{{ URL::to('/') . '/css/image-preview-input.css' }}">
 @endpush
 
-@extends('layouts.tenant', ['title' => __('Create brand')]) @section('content')
+@extends('layouts.tenant', ['title' => __('Create brand')])
+
+@section('content')
 
 <div class="lg:grid lg:grid-cols-12 lg:gap-x-5">
     <!-- LEFT FORM -->
     <div class="space-y-6 sm:px-6 lg:px-0 lg:col-span-8">
-        <form action="{{ route('tenant.brands.store') }}" method="POST" enctype="multipart/form-data">
+        <form id="brandForm" action="{{ route('tenant.brands.store') }}" method="POST" enctype="multipart/form-data">
             @csrf
             <div class="shadow sm:rounded-md sm:overflow-hidden">
                 <div class="bg-white py-6 px-4 space-y-6 sm:p-6">
                     <div class="grid grid-cols-3 gap-6">
                         <div class="col-span-12 sm:col-span-3">
-                            <label for="name"
-                                class="block text-sm font-medium text-gray-700">{{ __('label.Name') }}</label>
-                            <input type="text" name="name" id="name" autocomplete="name" value="{{ old('name') }}"
-                                class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                                placeholder="Brand name" />
-                            @error('name')
-                            <p class="mt-2 text-sm text-red-500">
-                                {{ $message }}
-                            </p>
-                            @enderror
+                            @include('layouts.snippets.fields', ['type'=>'text','label'=>'Name','placeholder'=>'Brand name','name'=>'name','value'=> '' ])
                         </div>
 
                         <div class="col-span-3">
                             <label for="description" class="block text-sm font-medium text-gray-700">
-                                {{ __('label.Description') }}
+                                {{ __('Description') }}
                             </label>
                             <div class="mt-1">
-                                <textarea id="description" name="description" rows="5"
-                                    class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm ">{{ old('description') }}</textarea>
+                                <textarea id="description" name="description" rows="5" class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm ">{{ old('description') }}</textarea>
                             </div>
                         </div>
                     </div>
@@ -42,54 +34,41 @@
                 <div class="bg-white py-6 px-4 space-y-6 sm:p-6">
                     <div>
                         <h3 class="text-lg leading-6 font-medium text-gray-900">
-                            Search engine listing preview
+                            {{ __('Search engine listing preview') }}
                         </h3>
                         <p class="mt-1 text-sm text-gray-500">
-                            Add a title and description to see how this brand might appear in a search engine
-                            listing.
+                            {{__('Add a title and description to see how this brand might appear in a search engine listing.')}}
                         </p>
                     </div>
 
                     <div class="col-span-12 sm:col-span-3">
-                        <label for="page_title"
-                            class="block text-sm font-medium text-gray-700">{{ __('label.Page title') }}</label>
-                        <input type="text" name="page_title" id="page_title" autocomplete="page_title"
-                            class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                            placeholder="Brand name" value="{{ old('page_title') }}" />
-                        @error('page_title')
-                        <p class="mt-2 text-sm text-red-500">
-                            {{ $message }}
-                        </p>
-                        @enderror
+                        @include('layouts.snippets.fields', ['type'=>'text','label'=>'Title','placeholder'=>'Page title','name'=>'page_title','value'=> '' ])
                     </div>
+
                     <div class="col-span-3">
                         <label for="seo_description" class="block text-sm font-medium text-gray-700">
-                            {{ __('label.Description') }}
+                            {{ __('Description') }}
                         </label>
                         <div class="mt-1">
-                            <textarea id="seo_description" name="seo_description" rows="5"
-                                class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">{{ old('seo_description') }}</textarea>
+                            <textarea id="seo_description" name="seo_description" rows="5" class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">{{ old('seo_description') }}</textarea>
                         </div>
                     </div>
 
                     <div class="col-span-3 sm:col-span-2">
                         <label for="slug" class="block text-sm font-medium text-gray-700">
-                            URL and handle
+                            {{ __('URL and handle') }}
                         </label>
                         <div class="mt-1 rounded-md shadow-sm flex">
-                            <span
-                                class="bg-gray-50 border border-r-0 border-gray-300 px-3 inline-flex items-center text-gray-500 sm:text-sm">
-                                {{ Request::getHost() . '/brands/' }}
+                            <span class="bg-gray-50 border border-r-0 border-gray-300 px-3 inline-flex items-center text-gray-500 sm:text-sm">
+                                {{ Request::getHost() . '/' . __('brands') . '/'}}
                             </span>
-                            <input type="text" name="slug" id="slug" autocomplete="slug"
-                                class="mt-1 block w-full border border-gray-300 shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                                value="{{ old('slug') }}" />
-                            @error('slug')
-                            <p class="mt-2 text-sm text-red-500">
-                                {{ $message }}
-                            </p>
-                            @enderror
+                            <input type="text" name="slug" id="slug" autocomplete="slug" class="mt-1 block w-full border border-gray-300 shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" value="{{ old('slug') }}" />
                         </div>
+                        @error('slug')
+                        <p class="mt-2 text-sm text-red-500">
+                            {{ $message }}
+                        </p>
+                        @enderror
                     </div>
                 </div>
             </div>
@@ -100,19 +79,17 @@
             <div class="bg-white py-6 px-4 space-y-6 sm:p-6">
                 <fieldset>
                     <legend class="text-base font-medium text-gray-900">
-                        Other options
+                        {{ __('Other options') }}
                     </legend>
                     <div class="mt-4 space-y-4">
                         <div class="flex items-start">
                             <div class="h-5 flex items-center">
-                                <input id="status" name="status" type="checkbox"
-                                    class="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded"
-                                    {{ old('status') ? 'checked' : '' }} value="1" />
+                                <input id="status" name="status" type="checkbox" class="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded" {{ old('status') ? 'checked' : '' }} value="1" />
                             </div>
                             <div class="ml-3 text-sm">
-                                <label for="status" class="font-medium text-gray-700">{{ __('label.Active') }}</label>
+                                <label for="status" class="font-medium text-gray-700">{{ __('Active') }}</label>
                                 <p class="text-gray-500">
-                                    Set this brand active in your store.
+                                    {{ __('Set this brand active in your store.') }}
                                 </p>
                             </div>
                         </div>
@@ -125,25 +102,23 @@
             <div class="bg-white py-6 px-4 space-y-6 sm:p-6">
                 <div>
                     <h3 class="text-base font-medium text-gray-900">
-                        Brand image
+                        {{ __('Brand image') }}
                     </h3>
                 </div>
                 <div class="mt-1 border-2 border-gray-300 border-dashed rounded-md px-6 pt-5 pb-6 flex justify-center">
                     <div class="file-upload">
-                        <button class="file-upload-btn" type="button"
-                            onclick="document.getElementById('image_url').click()">Add Image</button>
+                        <button class="file-upload-btn" type="button" onclick="document.getElementById('image_url').click()">{{ __('Add image') }}</button>
 
                         <div id="image-upload-wrap">
                             <input id="image_url" name="image_url" type='file' accept="image/*" />
                             <div class="drag-text">
-                                <h3>Drag and drop a file or select add Image</h3>
+                                <h3>{{ __('Drag and drop a file or select add Image') }}</h3>
                             </div>
                         </div>
                         <div id="file-upload-content">
                             <img id="file-upload-image" src="#" alt="your image" />
                             <div class="image-title-wrap">
-                                <button type="button" id="remove-image">Remove <span id="image-title">Uploaded
-                                        Image</span></button>
+                                <button type="button" id="remove-image">{{ __('Remove') }} <span id="image-title">{{ __('Uploaded image') }}</span></button>
                             </div>
                         </div>
                     </div>
@@ -159,15 +134,13 @@
         <div class="bg-white py-6 px-4 space-y-6 sm:p-6">
             <div class="flex justify-end">
                 <span class="inline-flex rounded-md shadow-sm">
-                    <a href="{{ route('tenant.brands.index') }}"
-                        class="py-1 px-4 border border-gray-300 rounded-md text-sm font-medium text-gray-700 hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800 transition duration-150 ease-in-out">
-                        Cancel
+                    <a href="{{ route('tenant.brands.index') }}" class="py-1 px-4 border border-gray-300 rounded-md text-sm font-medium text-gray-700 hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800 transition duration-150 ease-in-out">
+                        {{ __('Cancel') }}
                     </a>
                 </span>
                 <span class="ml-3 inline-flex rounded-md shadow-sm">
-                    <button type="submit"
-                        class="py-1 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 shadow-sm hover:bg-indigo-500 focus:outline-none focus:shadow-outline-blue focus:bg-indigo-500 active:bg-indigo-600 transition duration-150 ease-in-out">
-                        Save Brand
+                    <button type="submit" class="py-1 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 shadow-sm hover:bg-indigo-500 focus:outline-none focus:shadow-outline-blue focus:bg-indigo-500 active:bg-indigo-600 transition duration-150 ease-in-out">
+                        {{ __('Save brand') }}
                     </button>
                 </span>
             </div>
@@ -181,4 +154,24 @@
 @push('js')
 <script src="{{ URL::to('/') . '/js/image-preview-input.js' }}"></script>
 <script src="{{ URL::to('/') . '/js/string-slugger.js' }}"></script>
+<script>
+    $(document).ready(function() {
+        $("#brandForm").validate({
+            rules: {
+                name: {
+                    required: true
+                },
+                page_title: {
+                    required: true,
+                },
+                slug: {
+                    required: true,
+                },
+                image_url: {
+                    required: true,
+                },
+            }
+        });
+    });
+</script>
 @endpush
