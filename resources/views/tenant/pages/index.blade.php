1f6504bb6a521b4b336a515556feeba6cb6cb1c4
Lucas Vinicius <lucasvscs@outlook.com>
2021-11-05 20:36:07 -0300

    All entities delete modal refactoring

--- resources/views/tenant/pages/index.blade.php
+++ resources/views/tenant/pages/index.blade.php
@@ -2,8 +2,7 @@
 
 <div class="">
 	<div class="max-w-7xl mx-auto">
-		<a href="{{ route('tenant.pages.create') }}"
-			class="px-5 py-2 text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 active:bg-indigo-700 transition ease-in-out duration-150">
+		<a href="{{ route('tenant.pages.create') }}" class="px-5 py-2 text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 active:bg-indigo-700 transition ease-in-out duration-150">
 			{{ __('New page') }}
 		</a>
 		<div class="block mt-8">
@@ -15,16 +14,13 @@
 								@if ($pages->count() >= 1)
 								<thead class="bg-gray-50">
 									<tr>
-										<th scope="col"
-											class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+										<th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
 											{{ __('Name') }}
 										</th>
-										<th scope="col"
-											class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+										<th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
 											{{ __('Status') }}
 										</th>
-										<th scope="col"
-											class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+										<th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
 											{{ __('Actions') }}
 										</th>
 									</tr>
@@ -43,13 +39,11 @@
 										</td>
 										<td class="px-6 py-4 whitespace-nowrap">
 											@if ($page->status)
-											<span
-												class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+											<span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
 												{{ __('Active') }}
 											</span>
 											@else
-											<span
-												class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-red-100 text-red-800">
+											<span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-red-100 text-red-800">
 												{{ __('Inactive') }}
 											</span>
 											@endif
@@ -57,25 +51,17 @@
 										<td class="px-6 py-4 whitespace-nowrap">
 											<div class="flex items-center">
 												<a href="{{ route('tenant.pages.edit', ['page' => $page->id]) }}">
-													<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none"
-														viewBox="0 0 24 24" stroke="currentColor">
-														<path stroke-linecap="round" stroke-linejoin="round"
-															stroke-width="2"
-															d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
+													<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+														<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
 													</svg>
 												</a>
 
-												<form
-													action="{{ route('tenant.pages.destroy', ['page' => $page->id]) }}"
-													method="post" style="margin-top: 4px;">
+												<form action="{{ route('tenant.pages.destroy', ['page' => $page->id]) }}" method="post" style="margin-top: 4px;">
 													@csrf
 													@method('DELETE')
 													<button type="submit" class="delete-resource-button">
-														<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6"
-															fill="none" viewBox="0 0 24 24" stroke="currentColor">
-															<path stroke-linecap="round" stroke-linejoin="round"
-																stroke-width="2"
-																d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+														<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+															<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
 														</svg>
 													</button>
 												</form>
@@ -97,51 +83,6 @@
 	</div>
 </div>
 
-{{-- Delete confirmation modal --}}
-<div id="delete-modal" class="fixed z-10 inset-0 overflow-y-auto" aria-labelledby="modal-title" role="dialog"
-	aria-modal="true" style="display: none;">
-	<div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
-		<div id="outside-modal" class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" aria-hidden="true">
-		</div>
-		<span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
-		<div
-			class="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6">
-			<div class="sm:flex sm:items-start">
-				<div
-					class="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
-					<svg class="h-6 w-6 text-red-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-						stroke="currentColor" aria-hidden="true">
-						<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-							d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
-					</svg>
-				</div>
-				<div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-					<h3 class="text-lg leading-6 font-medium text-gray-900" id="modal-title">
-						{{ __('Delete page?') }}
-					</h3>
-					<div class="mt-2">
-						<p class="text-sm text-gray-500">
-							{{ __('Are you sure you want to delete this page? This action cannot be undone.') }}
-						</p>
-					</div>
-				</div>
-			</div>
-			<div class="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
-				<button id="confirm-delete" type="button"
-					class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm">
-					{{ __('Confirm') }}
-				</button>
-				<button id="cancel-button" type="button"
-					class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:w-auto sm:text-sm">
-					{{ __('Cancel') }}
-				</button>
-			</div>
-		</div>
-	</div>
-</div>
+@include('layouts.snippets.delete-modal', ['entity' => 'page'])
 
 @endsection
-
-@push('js')
-<script src="{{ URL::to('/') . '/js/delete-modal.js' }}"></script>
-@endpush
