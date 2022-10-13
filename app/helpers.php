7819339c59110bd03b5417440648f27c36a60fbe
Wallace Aguiar <wallacevr@gmail.com>
2022-10-13 17:10:53 -0300

    HelperSubmenu

--- app/helpers.php
+++ app/helpers.php
@@ -415,3 +415,55 @@ if (!function_exists('create_menu')) {
         ];
     }
 }
+
+if (!function_exists('buildMenu')) {
+    function buildMenu($id_menu, $parent = 0, $indent = 0)
+    {
+        $arr = Menu::find($id_menu);
+    
+        foreach($arr->allChildren as $menu)
+        {
+            
+        echo '<div class="py-5" x-data="{menu_<?='. $menu->id .' ?>:false}" @mouseover="menu_<?= '. $menu->id .' ?> = true" @mouseleave="menu_<?= '. $menu->id .'?> = false">
+            <div class="relative items-center cursor-pointer text-sm font-medium">
+                <a href="'. $menu->url .'" class=" px-6  inline-flex items-center font-medium">
+                    '. $menu->title .'';
+                    if (count($menu->children)>0){
+                        echo '<svg class="secundary-text ml-2 h-5 w-5 " xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                        <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                    </svg>';
+                    }
+
+                 
+               echo '</a>';
+        
+            if (count($menu->allChildren)==0 )
+            {
+             
+                    echo '<div x-show="menu_<?= $menu->id ?>" x-cloak class="absolute z-10 -ml-4 mt-3 transform px-2 w-32   sm:px-0 lg:ml-0 " style="min-width: 150px;">
+                    <div class="rounded-lg shadow-lg w-48">
+                        <div class=" grid bg-white px-5 py-2 ">
+                        
+                            <a href="'.$menu->url .'" class=" py-2  text-secundary px-5  ">
+                                '. $menu->title .'
+                            </a>
+                     
+                        </div>
+                    </div>
+                </div>';
+                
+               
+            }else{
+             
+                foreach($menu->allChildren as $submenu){
+                    
+                    buildMenu($submenu->id, $menu['parent_id'], $menu['sort_id'] + 1);
+                }
+              
+               
+            }
+            echo '</div>
+            </div>';
+        }
+    }
+}
