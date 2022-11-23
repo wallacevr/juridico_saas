fb289e92af3acf803a44db8dfd343fad941936c9
Wallace Aguiar <wallaceaguiar.developer@gmail.com>
2022-11-23 12:04:08 +0000

    InitialColors

--- resources/views/tenant/layout/color.blade.php
+++ resources/views/tenant/layout/color.blade.php
@@ -27,39 +27,39 @@
                               
                                 <div class="">
                                     
-                                    <input type="color" name="primary_color" id="color-picker" value="{{get_config('general/layout/primary_color')??"#FFFFFF"}}" class="mt-1  bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none sm:text-sm">
+                                    <input type="color" name="primary_color" id="color-picker" value="{{get_config('general/layout/primary_color')??"#1C3A4D"}}" class="mt-1  bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none sm:text-sm">
                                     <label>Background primario</label>
                                 </div>
                                 <div class="">
                                     
-                                    <input type="color" name="title_primary_color" id="color-picker" value="{{get_config('general/layout/title_primary_color')??"#FFFFFF"}}" class="mt-1  bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none sm:text-sm">
+                                    <input type="color" name="title_primary_color" id="color-picker" value="{{get_config('general/layout/title_primary_color')??"#000000"}}" class="mt-1  bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none sm:text-sm">
                                     <label>Texto primario</label>
                                 </div>
                                 <div class="">
                                     
-                                    <input type="color" name="title_primary_color_hover" id="color-picker" value="{{get_config('general/layout/title_primary_color_hover')??"#FFFFFF"}}" class="mt-1  bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none sm:text-sm">
+                                    <input type="color" name="title_primary_color_hover" id="color-picker" value="{{get_config('general/layout/title_primary_color_hover')??"#000000"}}" class="mt-1  bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none sm:text-sm">
                                     <label>Texto primario hover</label>
                                 </div>
                                 @include('layouts.snippets.divide')
                                 <div class="">
                                     
-                                    <input type="color" name="secundary_color" id="color-picker" value="{{get_config('general/layout/secundary_color')??"#FFFFFF"}}" class="mt-1  bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none sm:text-sm">
+                                    <input type="color" name="secundary_color" id="color-picker" value="{{get_config('general/layout/secundary_color')??" #FFFFFF"}}" class="mt-1  bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none sm:text-sm">
                                     <label>Background secundario</label>
                                 </div>
                                 <div class="">
                                     
-                                    <input type="color" name="title_secundary_color" id="color-picker" value="{{get_config('general/layout/title_secundary_color')??"#FFFFFF"}}" class="mt-1  bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none sm:text-sm">
+                                    <input type="color" name="title_secundary_color" id="color-picker" value="{{get_config('general/layout/title_secundary_color')??"#374151"}}" class="mt-1  bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none sm:text-sm">
                                     <label>Texto secundario</label>
                                 </div>
                                 <div class="">
                                     
-                                    <input type="color" name="title_secundary_color_hover" id="color-picker" value="{{get_config('general/layout/title_secundary_color_hover')??"#FFFFFF"}}" class="mt-1  bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none sm:text-sm">
+                                    <input type="color" name="title_secundary_color_hover" id="color-picker" value="{{get_config('general/layout/title_secundary_color_hover')??"##374151"}}" class="mt-1  bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none sm:text-sm">
                                     <label>Texto secundario hover</label>
                                 </div>
                                 @include('layouts.snippets.divide')
                                 <div class="">
                                     
-                                    <input type="color" name="background_footer" id="color-picker" value="{{get_config('general/layout/background_footer')??"#FFFFFF"}}" class="mt-1  bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none sm:text-sm">
+                                    <input type="color" name="background_footer" id="color-picker" value="{{get_config('general/layout/background_footer')??"#000000"}}" class="mt-1  bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none sm:text-sm">
                                     <label>Background rodapé</label>
                                 </div>
                                 <div class="">
@@ -68,20 +68,20 @@
                                 </div>
                                 @include('layouts.snippets.divide')
                                 <div class="">
-                                    <input type="color" name="text_price" id="color-picker" value="{{get_config('general/layout/text_price')??"#FFFFFF"}}" class="mt-1  bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none sm:text-sm">
+                                    <input type="color" name="text_price" id="color-picker" value="{{get_config('general/layout/text_price')??"#1C3A4D"}}" class="mt-1  bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none sm:text-sm">
                                     <label>Texto preço</label>
                                 </div>
                                 <div class="">
-                                    <input type="color" name="text_price_with_special" id="color-picker" value="{{get_config('general/layout/text_price_with_special')??"#FFFFFF"}}" class="mt-1  bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none sm:text-sm">
+                                    <input type="color" name="text_price_with_special" id="color-picker" value="{{get_config('general/layout/text_price_with_special')??"#1C3A4D"}}" class="mt-1  bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none sm:text-sm">
                                     <label>Texto preço tachado</label>
                                 </div>
                                 <div class="">
-                                    <input type="color" name="text_special_price" id="color-picker" value="{{get_config('general/layout/text_special_price')??"#FFFFFF"}}" class="mt-1  bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none sm:text-sm">
+                                    <input type="color" name="text_special_price" id="color-picker" value="{{get_config('general/layout/text_special_price')??"#1C3A4D"}}" class="mt-1  bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none sm:text-sm">
                                     <label>Texto preço especial</label>
                                 </div>
                                 @include('layouts.snippets.divide')
                                 <div class="">
-                                    <input type="color" name="background_add_cart" id="color-picker" value="{{get_config('general/layout/background_add_cart')??"#FFFFFF"}}" class="mt-1  bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none sm:text-sm">
+                                    <input type="color" name="background_add_cart" id="color-picker" value="{{get_config('general/layout/background_add_cart')??"#1C3A4D"}}" class="mt-1  bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none sm:text-sm">
                                     <label>Background adicionar ao carrinho</label>
                                 </div>
                                 <div class="">
@@ -89,7 +89,7 @@
                                     <label>Texto adicionar ao carrinho</label>
                                 </div>
                                 <div class="">
-                                    <input type="color" name="background_add_cart_hover" id="color-picker" value="{{get_config('general/layout/background_add_cart_hover')??"#FFFFFF"}}" class="mt-1  bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none sm:text-sm">
+                                    <input type="color" name="background_add_cart_hover" id="color-picker" value="{{get_config('general/layout/background_add_cart_hover')??"#376D90"}}" class="mt-1  bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none sm:text-sm">
                                     <label>Background adicionar ao carrinho hover</label>
                                 </div>
                               
